2425bfbce373430186f7a42db4547362fc87264a
anucoder <anaghats7@gmail.com>
2022-11-28 07:16:05 +0530

    payment apis added

--- Routes/APIRouter.js
+++ Routes/APIRouter.js
@@ -4,9 +4,7 @@ const router = express.Router();
 const product = require("../controllers/productsController");
 const category = require("../controllers/categoryController")
 const cart = require('../controllers/cartController');
-const cartModel = require("../models/cartModel");
-// const user = require('../controllers/UserController')
-// const payment = require("../controllers/PaymentController")
+const payment = require("../controllers/PaymentController")
 
 router.get("/", (request, response) => {
   response.status(200).send("Test");
@@ -28,11 +26,10 @@ router.post("/cart/update-qty",cart.updateItemQuantity)
 router.post("/cart/get-qty",cart.getItemQuantity)
 router.post("/cart/delete-item",cart.deleteItem)
 // //User
-// router.post('/api/sign-up',user.SignUp)
-// router.post("/api/login",user.Login)
+
 
 // //payment
-// router.post("/api/payment/gen-order",payment.getOrderId)
-// router.post("/api/payment/verify",payment.verifyPayment)
+router.post("/api/payment/gen-order",payment.getOrderId)
+router.post("/api/payment/verify",payment.verifyPayment)
 
 module.exports = router;
